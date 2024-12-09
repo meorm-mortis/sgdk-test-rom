4b51aa7f60080002889ede8c886ebfa731278e59
meorm morits <149721455+meorm-mortis@users.noreply.github.com>
2024-12-09 09:52:43 -0700

    Add files via upload

---
+++ res/resources.h
@@ -0,0 +1,7 @@
+#ifndef _RES_RESOURCES_H_
+#define _RES_RESOURCES_H_
+
+extern const u8 track1[134144];
+extern const Image meow;
+
+#endif // _RES_RESOURCES_H_
---
+++ res/resources.res
@@ -0,0 +1,2 @@
+XGM track1 "02 - Mission Briefing.vgm" 
+IMAGE meow "meow_meow_bitch.png" 0
