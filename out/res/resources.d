ea5138618c5b3232b0d0e713c53436e3b8598718
meorm morits <149721455+meorm-mortis@users.noreply.github.com>
2024-12-09 09:51:52 -0700

    Add files via upload

---
+++ out/res/resources.d
@@ -0,0 +1,3 @@
+out/res/resources.o: res/resources.res \
+C:/Users/Doctor\ Meow/sgdk\ projects/meow\ test/res/02\ -\ Mission\ Briefing.vgm \
+C:/Users/Doctor\ Meow/sgdk\ projects/meow\ test/res/meow_meow_bitch.png
